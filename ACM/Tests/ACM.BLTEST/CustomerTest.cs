4b9717ee83f39843edf9abb7eedbb1cde2d7710c
rohit <rohitj559@gmail.com>
2019-06-05 23:53:02 -0500

    Testing the class: Valid Values

---
+++ ACM/Tests/ACM.BLTEST/CustomerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTEST
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Rohit",
+                LastName = "Jagannath"
+            };
+            string expected = "Jagannath,Rohit";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ACM/Tests/ACM.BLTEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTEST")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b41aaeaf-d4b5-435d-b85b-d152c03a4464")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
