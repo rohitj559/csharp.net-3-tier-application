fe0bf173fa27f4ae2c879aacb97aeaaf9e0144b6
rohit <rohitj559@gmail.com>
2019-06-05 23:12:08 -0500

    Building a class: Properties

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string FirstName { get; set; }
+
+    }
+}
