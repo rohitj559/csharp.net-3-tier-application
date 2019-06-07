935733f232e4b692e9d1d532adb817baea089f28
rohit <rohitj559@gmail.com>
2019-06-06 19:08:48 -0500

    Building a class: Constructors

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -8,6 +8,16 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public Customer()
+        {
+
+        }
+
+        public Customer(int customerId)
+        {
+            CustomerId = customerId;
+        }
+
         public int CustomerId { get; private set; }
         public string Email { get; set; }
         public string FullName
